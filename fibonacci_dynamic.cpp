d3419631d3f6e00c7f240a06373c257662821395
Vivek Aseeja <vivekaseeja@me.com>
2014-05-04 11:24:59 -0700

    Fix corner case when n = 0

--- fibonacci_dynamic.cpp
+++ fibonacci_dynamic.cpp
@@ -9,18 +9,22 @@ unsigned int fibo(int n)
 	map<unsigned int,unsigned int>::iterator i = g_FiboCache.find(n); 
 	if (i != g_FiboCache.end())  
 		return i->second; 
-		
-	if (n <= 2) { 
-		g_FiboCache[n] = 1; 
-		return 1; 
+
+	if (n == 0) { 
+		g_FiboCache[0] = 0; 
+	} 
+	else if (n == 1) { 
+		g_FiboCache[1] = 1; 
+	} 
+	else {  	
+		g_FiboCache[n] = fibo(n-1) + fibo(n-2); 
 	} 
 
-	g_FiboCache[n] = fibo(n-1) + fibo(n-2); 
 	return g_FiboCache[n]; 
 } 
 
 int main() 
 { 
-	cout << fibo(10) << endl; 
+	cout << fibo(2) << endl; 
 	return 0; 
 } 
