1b3aef82e940148af536a46617a4e2d2db863249
Vivek Aseeja <vivekaseeja@me.com>
2014-05-04 11:35:03 -0700

    Simplify code by inserting 0 and 1 values

--- fibonacci_dynamic.cpp
+++ fibonacci_dynamic.cpp
@@ -2,29 +2,26 @@
 #include <map> 
 using namespace std; 
 
-static map<unsigned int, unsigned int> g_FiboCache; 
+static map<unsigned, unsigned> g_FiboCache; 
 
 unsigned int fibo(int n) 
 { 
-	map<unsigned int,unsigned int>::iterator i = g_FiboCache.find(n); 
+	map<unsigned,unsigned>::iterator i = g_FiboCache.find(n); 
 	if (i != g_FiboCache.end())  
 		return i->second; 
 
-	if (n == 0) { 
-		g_FiboCache[0] = 0; 
-	} 
-	else if (n == 1) { 
-		g_FiboCache[1] = 1; 
-	} 
-	else {  	
-		g_FiboCache[n] = fibo(n-1) + fibo(n-2); 
-	} 
+	g_FiboCache[n] = fibo(n-1) + fibo(n-2); 
 
 	return g_FiboCache[n]; 
 } 
 
 int main() 
-{ 
+{
+	g_FiboCache[0] = 0; 
+	g_FiboCache[1] = 1; 
+ 
 	cout << fibo(2) << endl; 
+	cout << fibo(10) << endl; 
+	cout << fibo(0) << endl; 
 	return 0; 
 } 
