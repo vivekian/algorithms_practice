094829725abc32f04acd399d62c4d7fea3883033
vivekian <vivekaseeja@me.com>
2014-06-02 08:45:32 -0700

    Add new methods - merge, deletion etc.

--- reverse_ll.cpp
+++ reverse_ll.cpp
@@ -4,70 +4,170 @@
 using namespace std; 
 
 typedef struct Node 
-{ 	
+{	
 	int item; 
 	Node* next;
 	
 	Node(int Item): item(Item),next(NULL) {}   
 } Node; 
 
-void Insert(Node* Head, int Item) 
+
+namespace LinkedList 
 { 
-	Node* curr = Head; 
+		void Insert(Node* Head, int Item) 
+		{ 
+			Node* curr = Head; 
 
-	while(curr->next) 
-		curr = curr->next; 
+			while(curr->next) 
+				curr = curr->next; 
+			
+			curr->next = new Node(Item); 
+		}
+
+		void Print(Node* Head) 
+		{ 
+			Node* curr = Head; 
+			
+			while(curr) { 
+				cout << curr->item << endl; 
+				curr = curr->next;
+			} 
+		}
+
+		void Reverse(Node** Head) 
+		{ 
+			Node* prev = *Head; 
+			Node* curr = prev->next; 
+
+			if (!curr) 
+				return; 
+
+			// Important to make the first node's next ptr NULL. 
+			prev->next = NULL; 
+
+			while(curr) { 
+				Node* tmp = curr->next; 
+				curr->next = prev; 
+				prev = curr; 
+				curr = tmp; 
+			} 
+			
+			*Head = prev;
+		}   
+
+		void Reverse_SubList(Node** Head, const int Start, const int End) 
+		{ 
+			
 	
-	curr->next = new Node(Item); 
-}
+		void Delete(Node** Head, int Item) 
+		{ 
+			Node* prev = *Head; 
+			Node* curr = prev->next; 
+		
+			if(!prev) 
+				return; 
 
-void Print(Node* Head) 
-{ 
-	Node* curr = Head; 
-	
-	while(curr) { 
-		cout << curr->item << endl; 
-		curr = curr->next;
-	} 
-}
+			if(prev->item == Item) { 
+				*Head = curr; 
+				delete prev; 
+				return; 
+			} 
 
-void Reverse(Node** Head) 
-{ 
-	Node* prev = *Head; 
-	Node* curr = prev->next; 
+			while (curr) { 
+				if (curr->item == Item) { 
+					prev->next = curr->next; 
+					delete curr; 
+					break; 
+				} 
+				prev = curr; 
+				curr = curr->next; 
+			} 
+		} 	
 
-	if (!curr) 
-		return; 
+		Node* Merge(Node* HeadA, Node* HeadB) 
+		{ 
+			if(!HeadA && !HeadB) 
+				return NULL; 
+			
+			if (!HeadA) 
+				return HeadB; 
+			
+			if (!HeadB) 	
+				return HeadA; 
 
-	// Important to make the first node's next ptr NULL. 
-	prev->next = NULL; 
+			Node* a = HeadA; 
+			Node* b = HeadB; 
+			Node* newHead = NULL; 			
 
-	while(curr) { 
-		Node* tmp = curr->next; 
-		curr->next = prev; 
-		prev = curr; 
-		curr = tmp; 
-	} 
-	
-	*Head = prev;
-}   
+			if (a->item > b->item) { 
+				newHead = b; 
+				b = b->next; 
+			} 
+			else { 
+				newHead = a; 
+				a = a->next; 
+			}
+
+			Node* tmp = newHead; 
+ 
+			while(a && b) { 
+				if (a->item > b->item) { 
+					tmp->next = b;
+					b = b->next;  
+				}
+				else { 
+					tmp->next = a;
+					a = a->next; 
+				}
+				tmp = tmp->next;  
+			} 
+
+			//Merge if list 1 is iterated, and list 2 remains.  
+			if(a)  
+				tmp->next = a; 
+
+			if(b)  
+				tmp->next = b; 
+	 
+			return newHead; 
+		} 	
+
+} 
 
 int main() 
 { 
 	Node head(1); 
 
-	Insert(&head, 2); 
-	Insert(&head, 3); 
-	Insert(&head, 4); 
-	Insert(&head, 5); 
-	Insert(&head, 6); 
+	LinkedList::Insert(&head, 2); 
+	LinkedList::Insert(&head, 3); 
+	LinkedList::Insert(&head, 4); 
+	LinkedList::Insert(&head, 5); 
+	LinkedList::Insert(&head, 6); 
 
-	Print(&head); 
+	LinkedList::Print(&head); 
 
 	Node* newhead = &head;
-	Reverse(&newhead); 
-	cout << endl; 
-	Print(newhead); 
+	LinkedList::Reverse(&newhead); 
 	
+	cout << endl; 
+	LinkedList::Print(newhead); 
+	
+	LinkedList::Delete(&newhead, 6); 
+	cout << endl; 
+	LinkedList::Print(newhead); 
+
+	Node h1(1); 
+	LinkedList::Insert(&h1, 3); 
+	LinkedList::Insert(&h1, 5); 
+	LinkedList::Insert(&h1, 7); 
+	Node h2(2);
+	LinkedList::Insert(&h2, 4); 
+	LinkedList::Insert(&h2, 6); 
+	LinkedList::Insert(&h2, 8); 
+
+	Node* h3 = LinkedList::Merge(&h1, &h2);  
+	cout << endl; 
+	LinkedList::Print(h3); 
+
 	return 0; 
 } 
