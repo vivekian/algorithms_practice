a4859cd9e8374c61d0d64643f3971ee0df252a50
vivekian <vivekaseeja@me.com>
2014-06-04 14:47:09 -0700

    Add GetNext() method to BST

--- binarytree.cpp
+++ binarytree.cpp
@@ -9,8 +9,9 @@ typedef struct Node
 	int item;
 	Node* left; 
 	Node* right; 
+	Node* parent; 
 	
-	Node(int Item): item(Item), left(NULL), right(NULL) {} 
+	Node(int Item): item(Item), left(NULL), right(NULL), parent(NULL)  {} 
 } Node; 
 
 Node* Insert(Node* Root, int Item) 
@@ -19,10 +20,14 @@ Node* Insert(Node* Root, int Item)
 		return new Node(Item); 
 	
 	if (Item <= Root->item) 
+	{ 
 		Root->left = Insert(Root->left, Item); 
-	else 
+		Root->left->parent = Root; 
+	} 
+	else { 
 		Root->right = Insert(Root->right, Item); 
-
+		Root->right->parent = Root;
+	} 
 	return Root; 
 }  
 
@@ -47,6 +52,67 @@ void Print(Node* Root)
 	Print(Root->left); 
 	cout << Root->item << endl; 
 	Print(Root->right); 
+}
+
+bool ValidateBST(Node* Root) 
+{ 
+	if (Root == NULL) 
+		return true; 
+
+	bool leftValid = false; 
+	bool rightValid = false; 	
+
+	if (Root->left)
+	{  
+		if (Root->left->item <= Root->item) 
+		{ 
+			leftValid = ValidateBST(Root->left); 
+		} 
+	} 
+	else 
+	{ 
+		leftValid = true; 
+	} 
+
+	if (Root->right) 
+	{ 
+		if (Root->right->item > Root->item) 
+		{ 
+			rightValid = ValidateBST(Root->right); 
+		}
+	} 
+	else 
+	{ 
+		rightValid = true;
+	} 
+
+	return leftValid && rightValid; 
+}  
+
+int FindMin(Node* Root) 
+{ 
+	if (Root == NULL) 
+		return -1; 
+
+	Node* t = Root; 
+
+	while(t->left) 
+		t = t->left; 
+
+	return t->item; 
+} 
+
+int FindMax(Node* Root) 
+{ 
+	if (Root == NULL) 
+		return -1; 
+	
+	Node* t = Root; 
+	
+	while(t->right) 
+		t = t->right; 
+	
+	return t->item; 
 } 
 
 int GetHeight(Node* Root) 
@@ -59,7 +125,6 @@ int GetHeight(Node* Root)
 	int lh = GetHeight(Root->left);
 	if (lh == -1) 
 		return -1; 	
- 	cout << "yes" <<endl; 	
 	int rh = GetHeight(Root->right); 
 	if (rh == -1) 
 		return -1; 
@@ -70,7 +135,68 @@ int GetHeight(Node* Root)
 		return max(lh, rh) + 1; 
 } 
 
+// If leaf left child - parent 
+// If leaf right child
+//		first parent's parent which is a left child 
+// If a node with children  
+//		left most child of a right subtree 
+ 
+int GetNext(Node* T, int Item)
+{
+	if (T == NULL) 
+		return -1; 
+	
+	if(T->item == Item) 
+	{ 
+		if(!T->left && !T->right) 
+		{ 
+			// if leaf is a left child 
+			if (T->parent->left == T) 
+			{  
+				return T->parent->item; 
+			} 
+			// if leaf is a right child 
+			else 
+			{ 
+				Node* next = T->parent; 
+				while(next && next->parent && (next != next->parent->left)) 
+					next = next->parent; 
+				
+				return (next->parent ? next->parent->item: -1); 
+			} 
+		} 
+		else 
+		{ 
+			return FindMin(T->right); 
+		}
+	} 
+	else if (Item < T->item) 
+	{ 
+		return GetNext(T->left, Item); 
+	} 
+	else 
+	{ 
+		return GetNext(T->right, Item); 
+	}  
+} 
 
+bool DoesPathSumExist(Node* Root, int Sum, const int PathSum) 
+{ 
+	if (Root == NULL)
+		return false; 
+
+	if (!Root->left && !Root->right) 
+	{ 
+		Sum += Root->item; 
+		return (Sum == PathSum); 
+	} 
+
+	return DoesPathSumExist(Root->left, Sum + Root->item, PathSum) || 
+		   DoesPathSumExist(Root->right, Sum + Root->item, PathSum); 
+} 
+
+
+	
 bool IsBalanced(Node* Root) 
 {
 	return (GetHeight(Root) != -1); 
@@ -78,25 +204,32 @@ bool IsBalanced(Node* Root)
 
 int main()
 { 
-	Node root(10); 
-	Insert(&root, 8);  
-	Insert(&root, 12);  
-	Insert(&root, 6);  
-	Insert(&root, 11);  
-	Insert(&root, 13);  
-	Insert(&root, 9);  
+	Node* root = NULL;  
+	root = Insert(root, 10);   
+	Insert(root, 8);  
+	Insert(root, 12);  
+	Insert(root, 6);  
+	Insert(root, 11);  
+	Insert(root, 13);  
+	Insert(root, 9);  
 
-	Print(&root); 
+	Print(root); 
 
-	cout << (Search(&root, 11) ? "Found" : "Not Found") << endl; 	
-	cout << (Search(&root, 23) ? "Found" : "Not Found") << endl; 	
-	cout << IsBalanced(&root) << endl; 
-
-	Node* root2 = Insert(root2, 1); 
-	Insert(root2, 2); 
-	Insert(root2, 3); 
+	cout << (Search(root, 11) ? "Found" : "Not Found") << endl; 	
+	cout << (Search(root, 23) ? "Found" : "Not Found") << endl; 	
+	cout << IsBalanced(root) << endl; 
 	
-	cout << IsBalanced(root2) << endl; 
+	cout << DoesPathSumExist(root, 0, 100) << endl; 
+	cout << DoesPathSumExist(root, 0, 24) << endl; 
+	cout << DoesPathSumExist(root, 0, 35) << endl; 
 
+	cout << ValidateBST(root) << endl; 
+	cout << FindMin(root) << endl; 
+	cout << FindMax(root) << endl;
+	cout << "11 " << GetNext(root, 11) << endl;  
+	cout << "9 " << GetNext(root, 9) << endl;  
+	cout << "13 " << GetNext(root, 13) << endl;  
+	cout << "8 " << GetNext(root, 8) << endl;  
+	cout << "10 " << GetNext(root, 10) << endl;  
 	return 0; 
 } 
