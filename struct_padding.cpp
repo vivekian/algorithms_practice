c86a32aceb362ccaaec6704be2e7e7aef26ee540
vivekian <vivekaseeja@me.com>
2014-06-20 10:55:04 -0700

    update with comments

--- struct_padding.cpp
+++ struct_padding.cpp
@@ -10,9 +10,14 @@ struct foo
 int main() 
 {
 	struct foo bar;  
-	int* y; 
+	int* y;
+
+	// although the total size is 5, compiler pads extra bytes to 8 
+	// this is done for alignment purposes.  
 	cout << sizeof(struct foo) << endl; 
 	cout << sizeof(bar.x) << endl; 
+	
+	// this is printing 8 bytes, because we are on a 64 bit machine 
 	cout << sizeof(y) << endl; 
 	return 0;
 }
