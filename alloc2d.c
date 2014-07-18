55a0b0b87b2ef8d381bb00fa0e3ca7d46267abfc
vivekian <vivekaseeja@me.com>
2014-07-18 15:51:17 -0700

    allocate a 2d array in C style

---
+++ alloc2d.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int** alloc2d(int rows, int cols) 
+{ 
+	int** matrix =  malloc (rows * (sizeof(int*))); 
+	
+	int i = 0; 
+ 	for( ; i < rows; i++) { 
+		matrix[i] = malloc (cols * (sizeof(int))); 
+	}  
+
+	return matrix; 
+} 
+
+int main() 
+{
+	int** matrix = alloc2d(10, 10); 
+
+	int i = 0; 
+	for ( ; i < 10; i++) 
+		free(matrix[i]); 
+
+	free(matrix); 
+	return 0; 
+} 
