fc0ff7002cddb36fa2c00380b3bcbb4e14aa7241
vivekian <vivekaseeja@me.com>
2014-07-29 20:42:21 -0700

    simple palindrome implementation

---
+++ palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int palindrome(const char* s) 
+{ 
+	const char* t = s; 
+
+	while(*t) { 
+		t++; 
+	}  
+ 	t--; 
+	
+	while(s <= t) { 
+		if (*s != *t) { 
+			return 0; 
+		} 
+		s++; 
+		t--; 
+	} 
+	
+	return 1; 
+} 
+
+int main() 
+{ 
+	printf("%d\n", palindrome("abba")); 
+	printf("%d\n", palindrome("aba")); 
+	printf("%d\n", palindrome("abad")); 
+	printf("%d\n", palindrome("a")); 
+	return 0; 
+} 
+
