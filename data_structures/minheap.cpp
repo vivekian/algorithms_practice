ae7bb0e14261b9bd9bd643d2d14807fbb86eff1c
vivekian <vivekaseeja@me.com>
2016-02-22 11:30:19 -0800

    update min heap implementation with extract_min method

--- data_structures/minheap.cpp
+++ data_structures/minheap.cpp
@@ -10,6 +10,44 @@ public:
 
     ~PriorityQueue() {}
 
+    void insert(const int x) 
+    { 
+        if (n >= MAX_SIZE) 
+            return; 
+        else { 
+            n++; 
+            q[n] = x;
+            bubble_up(n);  
+        }
+    }
+
+    int getmin() 
+    { 
+        return q[1]; 
+    }
+
+    int extract_min() 
+    {
+        if (n == 0) 
+           return -1; 
+
+        int min = q[1]; 
+        q[1] = q[n]; 
+        --n; 
+        
+        bubble_down(1); 
+        return min; 
+    }
+
+
+    void print() 
+    { 
+        for (int i=1; i<=n; ++i) 
+            cout << q[i] << " "; 
+        cout << endl; 
+    }
+
+private: 
     int parent(const int child) 
     { 
         // reached the root of the tree, return -1
@@ -17,6 +55,11 @@ public:
             return -1;
         else 
             return static_cast<int>(child/2); 
+    }
+
+    int child(const int parent) 
+    { 
+        return static_cast<int>(parent * 2); 
     }
 
     void swap(int a, int b) 
@@ -38,30 +81,24 @@ public:
          }
     }
 
-    void insert(const int x) 
+    void bubble_down(int p) 
     { 
-        if (n >= MAX_SIZE) 
-            return; 
-        else { 
-            n++; 
-            q[n] = x;
-            bubble_up(n);  
+        int c = child(p); 
+        int min_idx = p; 
+
+        if ((c <= n) && q[min_idx] > q[c])
+            min_idx = c; 
+
+        if ((c+1 <= n) && q[min_idx] > q[c+1])
+            min_idx = c+1; 
+
+        if (min_idx != p) { 
+            swap(p, min_idx);
+            bubble_down(min_idx); 
         }
     }
 
-    int getmin() 
-    { 
-        return q[1]; 
-    }
 
-    void print() 
-    { 
-        for (int i=1; i<=n; ++i) 
-            cout << q[i] << " "; 
-        cout << endl; 
-    }
-
-private: 
     int n; 
     int q[MAX_SIZE];
 };
@@ -75,6 +112,14 @@ int main()
         pq.insert(x); 
 
     pq.print(); 
+
+    int item; 
+
+    do { 
+        item = pq.extract_min(); 
+        pq.print(); 
+    } while(item != -1); 
+
 
     return 0; 
 }
