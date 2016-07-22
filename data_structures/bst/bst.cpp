1f69f792a03a5880db440eb21585b1dd7320fb9f
vivekian <vivekaseeja@me.com>
2016-07-22 09:15:40 -0700

    [BST] fix FindKey method to use raw pointers

--- data_structures/bst/bst.cpp
+++ data_structures/bst/bst.cpp
@@ -28,6 +28,18 @@ unique_ptr<BST_node<T>> Insert(unique_ptr<BST_node<T>> head, const T& item)
     return head; 
 }
 
+template <typename T>
+void InorderTraversal(unique_ptr<BST_node<T>>& head)
+{ 
+    if (head->left) 
+        InorderTraversal(head->left); 
+
+    cout << head->data << " "; 
+
+    if (head->right) 
+        InorderTraversal(head->right); 
+}
+
 template <typename T> 
 bool Search(const unique_ptr<BST_node<T>>& head, const T& item) 
 { 
@@ -42,6 +54,52 @@ bool Search(const unique_ptr<BST_node<T>>& head, const T& item)
     return false; 
 }
 
+template <typename T> 
+BST_node<T>* FindKey(unique_ptr<BST_node<T>>& head, const T& item)
+{
+   BST_node<T>* tmp = head.get();
+
+   while (tmp) { 
+      if (tmp->data == item) 
+         return tmp; 
+
+      if (item < tmp->data) 
+          tmp = tmp->left.get(); 
+      else  
+          tmp = tmp->right.get(); 
+   }
+
+   return nullptr;    
+}
+
+template <typename T> 
+void FindKLargestItems(unique_ptr<BST_node<T>>& head, int k, vector<T>& items)
+{
+   unique_ptr<BST_node<T>> &tmp = head; 
+
+   if (tmp->right && items.empty()) 
+        FindKLargestItems(tmp->right, k, items); 
+
+   cout << "pushing " << tmp->data << endl; 
+   if (items.size() != k) 
+       items.emplace_back(tmp->data); 
+} 
+
+template <typename T>
+bool IsBST(const unique_ptr<BST_node<T>>& head, T lower, T higher) 
+{
+    bool isLeft, isRight; 
+
+    if (!head) 
+        return true; 
+
+    if (head->data < lower || head->data > higher) 
+        return false; 
+
+    return (IsBST(head->left, lower, head->data) && 
+            IsBST(head->right, head->data, higher)); 
+} 
+
 int main(int argc, char ** argv)
 {
     vector<uint32_t> nums = {10, 20, 8, 35, 6, 22, 3}; 
@@ -50,8 +108,25 @@ int main(int argc, char ** argv)
     for (const auto &num: nums)
         head = Insert(move(head), num);  
 
+    InorderTraversal(head); 
+    cout << endl; 
+
     cout << Search(head, uint32_t(8)) << endl; 
     cout << Search(head, uint32_t(36)) << endl; 
+    cout << IsBST(head, numeric_limits<uint32_t>::min(), numeric_limits<uint32_t>::max()) << endl; 
+
+    InorderTraversal(head);  
+    cout << endl;
+
+    cout << FindKey(head, uint32_t(8))->data << endl; 
+   
+    InorderTraversal(head);  
+    cout << endl; 
+    vector<uint32_t> items; 
+    FindKLargestItems(head, 3, items); 
+
+    for (const auto& item: items) 
+       cout << item << endl;  
 
     return 0; 
 } 
