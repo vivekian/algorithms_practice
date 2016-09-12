ce15423590e756b8bfb4f2ddb513890035dae9f2
vivekian <vivekaseeja@me.com>
2016-09-12 12:40:18 -0700

    [trees] test node deletion with examples

--- data_structures/trees/bst_delete_node.cpp
+++ data_structures/trees/bst_delete_node.cpp
@@ -22,7 +22,8 @@ bst_node* make_tree()
     root->left->left = new bst_node(2); 
     root->left->right = new bst_node(9); 
     root->right->left = new bst_node(15); 
-    root->right->right = new bst_node(19); 
+    root->right->right = new bst_node(20); 
+    root->right->right->left = new bst_node(19); 
 
     root->right->left->right = new bst_node(17); 
     root->right->left->left = new bst_node(13); 
@@ -42,27 +43,40 @@ void assign_child_to_parent(bst_node* parent, bst_node* child, bool ischildright
 
 void delete_node_helper(bst_node* parent, bst_node* delnode) 
 {
-    bool ischildright = parent->right; 
+    cout << "deleting node with value " << delnode->value << endl; 
+    bool ischildright = parent->right == delnode; 
 
+    // please see page 297 of introduction to algorithms for all cases 
+    // if right child does not exist, then promote left child of the node to be deleted 
     if(!delnode->right) { 
         assign_child_to_parent(parent, delnode->left, ischildright); 
         return; 
-    } 
+    }
+    // if left child does not exist, then promote right child of the node to be deleted  
     else if (!delnode->left) { 
         assign_child_to_parent(parent, delnode->right, ischildright); 
         return; 
-    } 
-    else { 
+    }
+    // if both children exist for the node to be deleted  
+    else {
+        // if the right child's left child does not exist for the node to be deleted. 
+        // then assign promote the right child assign the node to be deleted's left child 
+        // to the empty left pointer.  
         if (!delnode->right->left) { 
             assign_child_to_parent(parent, delnode->right, ischildright); 
+            delnode->right->left = delnode->left; 
         } 
+        // this is the complex case. 
         else { 
             auto tmp = delnode->right->left; 
             delnode->right->left = tmp->right; 
             tmp->right = delnode->right; 
+            tmp->left = delnode->left; 
             assign_child_to_parent(parent, tmp, ischildright);   
         } 
     } 
+
+    delete delnode; 
 } 
 
 bool delete_node(bst_node* root, bst_node* delnode) 
@@ -117,6 +131,14 @@ void write_bst(ostream& os, const bst_node* root)
 int main() 
 { 
     bst_node* root = make_tree(); 
+    write_bst(cout, root); 
+    delete_node(root, root->left->left);
+    cout << endl; 
+    write_bst(cout, root);  
+    cout << endl; 
+    delete_node(root, root->right->left); 
+    write_bst(cout, root); 
+    delete_node(root, root->right); 
     write_bst(cout, root); 
     return 0; 
 } 
