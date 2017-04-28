a4a49ce2cff38457f7bfd0a560e0d117592a2620
vivekian <vivekaseeja@me.com>
2017-04-28 16:29:19 -0700

    inorder traversal of binary tree (non-recursive)

---
+++ data_structures/trees/bt_inorder_traversal.cpp
@@ -0,0 +1,70 @@
+// This is solution to problem 144
+
+#include <iostream> 
+#include <vector> 
+#include <stack> 
+
+using namespace std; 
+
+/**
+ * Definition for a binary tree node.*/ 
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+		vector<int> res = {}; 
+		stack<TreeNode*> nodes; 
+		TreeNode* curr = root; 
+    
+        while (curr || !nodes.empty()) { 
+            if (!curr) { 
+				curr = nodes.top(); 
+				nodes.pop(); 
+                res.push_back(curr->val); 
+                curr = curr->right; 
+			} 
+			
+            if (curr->left) {
+                nodes.push(curr);  
+                curr = curr->left; 
+            }
+            else { 
+			    res.push_back(curr->val); 
+                curr = curr->right; 
+            }
+		} 	
+
+   		return res;  
+    }
+};
+
+TreeNode* makeTree() 
+{ 
+	TreeNode* root = new TreeNode(1); 
+	root->left = new TreeNode(2); 
+	root->left->left = new TreeNode(4); 
+	root->left->right = new TreeNode(5); 
+	root->right = new TreeNode(3); 
+	root->right->left = new TreeNode(6); 
+	root->right->right = new TreeNode(7); 
+	return root; 
+} 
+
+int main() 
+{ 
+	Solution solution; 
+	TreeNode* root = makeTree(); 
+	auto sol = solution.preorderTraversal(root); 
+
+	for (auto s: sol) { 
+		cout << s << ","; 
+	} 
+
+	return 0; 
+} 
