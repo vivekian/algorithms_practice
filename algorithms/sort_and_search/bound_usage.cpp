870f12954f2db7fee3ed5236cc4d2a9e37478f63
vivekian <vivekaseeja@me.com>
2017-05-09 17:18:54 -0700

    [bound usage] minor updates

--- algorithms/sort_and_search/bound_usage.cpp
+++ algorithms/sort_and_search/bound_usage.cpp
@@ -9,12 +9,17 @@ using namespace std;
 void test_upper_bound() 
 { 
     const vector<uint32_t> nums = {1, 3, 5, 7, 9, 11, 23, 55, 66, 89}; 
-    
+
+    // find the first number which is greater than 23     
     auto item = upper_bound(nums.begin(), nums.end(), 23);
     cout << "item greater than 23 is:" << *item << endl; 
     
+    // find the first number which is equal or greater to 24
     item = lower_bound(nums.begin(), nums.end(), 24); 
     cout << "lower_bound for 24 is:" << *item << endl;
+
+    item = lower_bound(nums.begin(), nums.end(), 23); 
+    cout << "lower_bound for 23 is:" << *item << endl;
 } 
 
 int main() 
