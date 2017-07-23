b066d96e23d01ecad36e699cf81bf3ca2e0dd3a3
vivekian <vivekaseeja@me.com>
2017-07-23 12:41:59 -0700

    solution to problem 646 of leetcode

---
+++ algorithms/dynamic_programming/max_length_chain.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+
+using namespace std; 
+
+inline bool isvalid (vector<int>& a, vector<int>& b) { 
+	return (a[1] < b[0]);
+}
+    
+int findLongestChain(vector<vector<int>>& pairs) {
+	
+	auto cmp = [](const vector<int>& p1, const vector<int>& p2) { return p1[0] < p2[0];};
+	sort(pairs.begin(), pairs.end(), cmp); 
+	
+	int len = pairs.size(); 
+	vector<int> max_cnt(len, 0); 
+	int global_max = 0; 
+
+	for (int i=len-1; i >= 0; --i) { 
+		for (int j=i; j<len; ++j) { 
+			if (isvalid(pairs[i], pairs[j])) { 
+				max_cnt[i] = max(max_cnt[i], max_cnt[j] + 1); 
+				global_max = max(max_cnt[i], global_max); 
+			} 
+		}
+	}
+
+	return global_max; 	
+}
+
+int main() 
+{ 
+	vector<vector<int>> pairs = {{1,2},{3,4},{3,5},{5,6},{6,7},{8,9}}; 
+	cout << findLongestChain(pairs) << endl;
+
+	return 0; 
+} 
