63608f1fcd0a4527d8858802f386b8f316387290
vivekian <vivekaseeja@me.com>
2017-09-23 16:47:48 -0700

    notes on the friend circles question

--- data_structures/graphs/friend_circles.cpp
+++ data_structures/graphs/friend_circles.cpp
@@ -9,7 +9,10 @@ using namespace std;
 
 int findCircleNum(vector<vector<int>>& M)
 {
+    // final count of the total number of friend circles.
     int count=0; 
+
+    // if a friend is already visited. 
     vector<bool> isVisited (M.size(), false);
     queue<int> tovisit; 
 
@@ -19,13 +22,19 @@ int findCircleNum(vector<vector<int>>& M)
             tovisit.push(i);
             ++count; 
 
+            // once all friends are visited in a circle, then this while loop
+            // will exit and we will search for another group in the for loop above.
+            // this is really a connected components problem using BFS.
             while(!tovisit.empty()) { 
+                // pop a friend from the front of the queue 
                 int mate = tovisit.front();
                 tovisit.pop();
+                // mark the friend visited 
                 isVisited[mate] = true;
 
+                // for all the friends in the array 
                 for (int j=0; j<M.size(); ++j) { 
-                    
+                    // if a direct friend and not visited, push to queue to explore 
                     if ((M[mate][j] == 1) && (!isVisited[j]) && (mate != j)) {
                        tovisit.push(j);
                     }
